dd689269ab3d507462d90f4433087e8b1f38ec4a
Andrey <sergeev.sergeev-and@yandex.ru>
2022-04-28 17:43:38 +0700

    Homework_2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход
+//три числа и выдает максимальное из этих чисел
+
+Console.WriteLine("Введите три целых числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if(a < b) max = b;
+if(b < c) max = c;
+
+Console.WriteLine("Наибольшее число");
+Console.WriteLine(max);
+
+
