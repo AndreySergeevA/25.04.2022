0364ee9600e6a51e8dd4bb91d094552d05d7a4d4
Andrey <sergeev.sergeev-and@yandex.ru>
2022-04-28 17:50:57 +0700

    Homework_3

---
+++ Homework_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает
+//число и выдает, является ли число четным
+
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
